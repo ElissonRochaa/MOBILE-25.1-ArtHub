913da99c8f22450bbe0056eaa24bf3658f533b6e
Pedro-RGS <pedro.gsouza@upe.br>
2025-04-27 20:05:37 -0300

    feat: começo do desenvolvimento da tela de visualizar post

--- lib/pages/tela_post.dart
+++ lib/pages/tela_post.dart
@@ -2,8 +2,62 @@ import 'package:arthub/widgets/barra_pesquisa_widget.dart';
 import 'package:arthub/widgets/lista_filtros_widget.dart';
 import 'package:flutter/material.dart';
 
-class TelaPost extends StatelessWidget {
+class TelaPost extends StatefulWidget {
   const TelaPost({super.key});
+
+  @override
+  State<TelaPost> createState() => _TelaPostState();
+}
+
+class _TelaPostState extends State<TelaPost> {
+  bool isCurtido = false;
+  Widget post(BuildContext context){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 250,
+          child: Column(
+            children: [
+              Container(
+                width: 335,
+                height: 335,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/cat.jpeg'),
+                    fit: BoxFit.fill
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("@esnupi"),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () => {
+                          setState(() {
+                            isCurtido = !isCurtido;
+                          })
+                        },
+                        child: isCurtido ? Icon(Icons.favorite_rounded) : Icon(Icons.favorite_border_rounded),
+                      ),
+                      GestureDetector(
+                        onTap: () => {print("Botão de compartilhar foi clicado")},
+                        child: Icon(Icons.share),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +68,19 @@ class TelaPost extends StatelessWidget {
         title: BarraPesquisaWidget()),
       body: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               ListaFiltrosWidget(),
+              IconButton(
+                  onPressed: () => {print("Botão de voltar apertado")},
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Theme.of(context).colorScheme.primary,
+                  )
+              ),
+              SingleChildScrollView(
+                child: post(context),
+              ),
             ],
           )
         ),
