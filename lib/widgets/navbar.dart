8a6e354694eb1da72e5ef91ede963ceaf87e6598
paula <paula.beatriz@upe.br>
2025-04-27 18:56:56 -0300

    feat: mudanca da navbar de stateless para stateful

--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 
-class Navbar extends StatelessWidget {
+class Navbar extends StatefulWidget {
   const Navbar({super.key});
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return NavigationBar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
       destinations: [
-        NavigationDestination(icon: Icon(Icons.search), label: ''),
-        NavigationDestination(icon: Icon(Icons.home), label: ''),
-        NavigationDestination(icon: Icon(Icons.person), label: ''),
+        NavigationDestination(
+          icon: Icon(
+            Icons.search,
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+          label: '',
+        ),
+        NavigationDestination(
+          icon: Icon(
+            Icons.home,
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+          label: '',
+        ),
+        NavigationDestination(
+          icon: Icon(
+            Icons.person,
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+          label: '',
+        ),
       ],
     );
   }
