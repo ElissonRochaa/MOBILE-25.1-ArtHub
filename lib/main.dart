1078a162430cd9c50a036ab3986eaa2c442ba71f
paula <paula.beatriz@upe.br>
2025-04-27 14:43:57 -0300

    feat: adicao do AppBar e configuracoes do ambiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
-import 'package:arthub/config/themeApp.dart';
 import 'package:flutter/material.dart';
+import 'config/themeApp.dart';
+import 'pages/tela_inicial.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ArtHub',
+      debugShowCheckedModeBanner: false,
       theme: ThemeApp.theme,
+      title: 'ArtHub',
+      home: TelaInicial(),
     );
   }
 }
