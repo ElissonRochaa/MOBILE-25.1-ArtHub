a5ec2f7f59cd7b31e8a9147ce98fc13762e9b6d6
Pedro-RGS <pedro.gsouza@upe.br>
2025-04-26 22:54:41 -0300

    config: definição do theme do app e limpeza do main.dart

---
+++ lib/config/themeApp.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+class ThemeApp{
+  static final theme = ThemeData(
+    colorScheme: ColorScheme(
+        brightness: Brightness.light,
+        primary: Color(0xFFFFB246),
+        onPrimary: Colors.black,
+        secondary: Color(0xFFFEFF99),
+        onSecondary: Color(0xFF49454F),
+        tertiary: Color(0xFFC98311),
+        error: Colors.red,
+        onError: Colors.white,
+        surface: Colors.grey[200]!,
+        onSurface: Colors.black
+    )
+  );
+}
