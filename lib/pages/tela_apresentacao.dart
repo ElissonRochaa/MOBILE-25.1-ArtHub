0f503be9b6c3f9b1e98f901d08cb4109444d6bc8
Murilo Lucena <muriloglucenacanal@gmail.com>
2025-05-01 17:57:25 -0300

    refactor: estruturação de um stack para página de apresentação

--- lib/pages/tela_apresentacao.dart
+++ lib/pages/tela_apresentacao.dart
@@ -7,33 +7,70 @@ class TelaApresentacao extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment:
-              MainAxisAlignment.center, // Centraliza os widgets verticalmente
-          children: [
-            ImageFiltered(
-              imageFilter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/upe_entrada.jpeg'),
-                    colorFilter: ColorFilter.mode(
-                      Theme.of(context).colorScheme.primary,
-                      BlendMode
-                          .modulate, //Por enquanto utilizaremos a mescalgem de cor modulate, rever depois!
-                    ),
-                    fit: BoxFit.cover,
-                  ),
+    return Stack(
+      children: [
+        ImageFiltered(
+          imageFilter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/upe_entrada.jpeg'),
+                colorFilter: ColorFilter.mode(
+                  Theme.of(context).colorScheme.primary,
+                  BlendMode.modulate,
                 ),
+                fit: BoxFit.cover,
               ),
             ),
-          ],
+          ),
         ),
-      ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 264,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    //Serve pra dar um espaçamento entre o texto e a borda
+                    padding: const EdgeInsets.all(36),
+                    child: Text(
+                      "Um universo de arte na palma da sua mão.",
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      // Image.asset(
+                      //   'assets/images/upe_logo.png',
+                      //   width: 74,
+                      //   height: 33,
+                      // ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
