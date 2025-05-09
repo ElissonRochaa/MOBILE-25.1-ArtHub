b0bbe3760fa39771c7f7997b1e64083d4ab78498
Pedro-RGS <pedro.gsouza@upe.br>
2025-05-08 21:59:18 -0300

    refactor: tela_registro.dart foi refatorada para serguir os padrões das outras telas

--- lib/pages/tela_registro.dart
+++ lib/pages/tela_registro.dart
@@ -1,3 +1,4 @@
+import 'package:arthub/widgets/botao_voltar_widget.dart';
 import 'package:arthub/widgets/input_texto.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -10,98 +11,124 @@ class TelaRegistro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.primary,
       body: Stack(
         children: [
-          SingleChildScrollView(
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 40),
-                    InputTexto(
-                      label: "Nome",
-                      hintLabel: "Digite seu nome completo",
-                    ),
-                    const SizedBox(height: 20),
-                    InputTexto(
-                      label: "Apelido",
-                      hintLabel: "Digite seu apelido",
-                    ),
-                    const SizedBox(height: 20),
-                    InputTexto(
-                      label: "Data de Nascimento",
-                      hintLabel: "Digite sua data de nascimento",
-                      inputTipo: TextInputType.datetime,
-                    ),
-                    const SizedBox(height: 20),
-                    InputTexto(label: "Email", hintLabel: "Digite seu email"),
-                    const SizedBox(height: 20),
-                    InputTexto(label: "Senha", hintLabel: "Digite sua senha"),
-                    const SizedBox(height: 40),
-                    ElevatedButton(
-                      onPressed: () => {Navigator.pushNamed(context, "/login")},
-                      style: ElevatedButton.styleFrom(
-                        fixedSize: Size(165, 46),
-                        backgroundColor: ThemeApp.theme.colorScheme.tertiary,
-                        elevation: 6,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40),
+                topRight: Radius.circular(40),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 10,
+            top: 10,
+            child: BotaoVoltarWidget()
+            // IconButton.filled(
+            //   onPressed: () => {Navigator.pop(context)},
+            //   style: IconButton.styleFrom(backgroundColor: Colors.white),
+            //   icon: Icon(
+            //     Icons.arrow_back,
+            //     color: Theme.of(context).colorScheme.primary,
+            //   ),
+            // ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 60),
+            child: SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 40),
+                      InputTexto(
+                        label: "Nome",
+                        hintLabel: "Digite seu nome completo",
+                      ),
+                      const SizedBox(height: 20),
+                      InputTexto(
+                        label: "Apelido",
+                        hintLabel: "Digite seu apelido",
+                      ),
+                      const SizedBox(height: 20),
+                      InputTexto(
+                        hintLabel: "Data de Nascimento",
+                        label: "Data de Nascimento",
+                        inputTipo: TextInputType.datetime,
+                      ),
+                      const SizedBox(height: 20),
+                      InputTexto(label: "Email", hintLabel: "Digite seu email"),
+                      const SizedBox(height: 20),
+                      InputTexto(label: "Senha", hintLabel: "Digite sua senha"),
+                      const SizedBox(height: 40),
+                      ElevatedButton(
+                        onPressed: () => {Navigator.pushNamed(context, "/login")},
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: Size(165, 46),
+                          backgroundColor: ThemeApp.theme.colorScheme.tertiary,
+                          elevation: 6,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        child: Text(
+                          'Cadastrar-se',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            shadows: [
+                              Shadow(
+                                color: Color.fromRGBO(10, 10, 10, 0.3),
+                                offset: Offset(0, 3),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                      child: Text(
-                        'Cadastrar-se',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          shadows: [
-                            Shadow(
-                              color: Color.fromRGBO(10, 10, 10, 0.3),
-                              offset: Offset(0, 3),
-                              blurRadius: 2.0,
+                      const SizedBox(height: 80),
+                      RichText(
+                        text: TextSpan(
+                          text: "Já é Cadastrado? ",
+                          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "Faça o Login!",
+                              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                                color: Theme.of(context).colorScheme.onPrimary,
+                                decoration: TextDecoration.underline,
+                              ),
+                              recognizer:
+                                  TapGestureRecognizer()
+                                    ..onTap = () {
+                                      Navigator.pushNamed(context, '/login');
+                                    },
                             ),
                           ],
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 80),
-                    RichText(
-                      text: TextSpan(
-                        text: "Já é Cadastrado? ",
-                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                          color: Theme.of(context).colorScheme.onPrimary,
-                        ),
-                        children: [
-                          TextSpan(
-                            text: "Faça o Login!",
-                            style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                              decoration: TextDecoration.underline,
-                            ),
-                            recognizer:
-                                TapGestureRecognizer()
-                                  ..onTap = () {
-                                    Navigator.pushNamed(context, '/login');
-                                  },
+                      Container(
+                        margin: const EdgeInsets.only(top: 35),
+                        width: 75,
+                        height: 33,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.primaryContainer,
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/logo_upe.png'),
+                            fit: BoxFit.cover,
                           ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(top: 35),
-                      width: 75,
-                      height: 33,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primaryContainer,
-                        image: DecorationImage(
-                          image: AssetImage('assets/images/logo_upe.png'),
-                          fit: BoxFit.cover,
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
