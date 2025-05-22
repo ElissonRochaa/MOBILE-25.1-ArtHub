193e42a3881c6bdf50cd96b8bdd3256d2c937f04
laahnanes <larahnanes160@gmail.com>
2025-05-22 16:40:27 -0300

    feat: criação do enum de categoria

---
+++ lib/enums/categoria_enum.dart
@@ -0,0 +1,8 @@
+enum CategoriaEnum {
+  poema,
+  musica,
+  pintura,
+  desenho, 
+  escultura,
+  fotografia
+}
--- lib/models/publicacao_model.dart
+++ lib/models/publicacao_model.dart
@@ -1,17 +1,24 @@
+import 'package:arthub/enums/categoria_enum.dart';
 import 'package:arthub/enums/tipo_arquivo_enum.dart';
+import 'package:arthub/models/perfil_model.dart';
 
 class PublicacaoModel {
   final int id;
   final TipoArquivoEnum tipoArquivo;
+  final CategoriaEnum categoria;
+  final PerfilModel perfil;
   final DateTime dataPublicacao;
   final String legenda;
   final String nomeConteudo;
   final String titulo;
   final int curtidas;
 
+
   PublicacaoModel({
     required this.id,
     required this.tipoArquivo,
+    required this.categoria,
+    required this.perfil,
     required this.dataPublicacao,
     required this.legenda,
     required this.nomeConteudo,
@@ -23,6 +30,8 @@ class PublicacaoModel {
     return PublicacaoModel(
     id: json['id'], 
     tipoArquivo: json['tipoArquivo'], 
+    categoria: json['categoria'],
+    perfil: PerfilModel.fromJson(json['perfil']),
     dataPublicacao: json['dataPublicacao'], 
     legenda: json['legenda'], 
     nomeConteudo: json['nomeConteudo'], 
@@ -35,6 +44,8 @@ class PublicacaoModel {
     return {
       'id': id,
       'tipoArquivo': tipoArquivo,
+      'categoria': categoria,
+      'perfil': perfil.toJson(),
       'dataPublicacao': dataPublicacao,
       'legenda': legenda,
       'nomeConteudo': nomeConteudo,
