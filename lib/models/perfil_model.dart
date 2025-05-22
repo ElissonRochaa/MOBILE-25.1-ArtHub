eaf5c8ce5b0242f8711cba13e3045618408f90ac
paula <paula.beatriz@upe.br>
2025-05-22 16:20:09 -0300

    feat: models de perfil e usuario

---
+++ lib/models/perfil_model.dart
@@ -0,0 +1,51 @@
+class PerfilModel {
+  final int id;
+  final String biografia;
+  final String fotoPerfil;
+  final String banner;
+  final UsuarioModel usuario;
+  final List<PublicacaoModel> publicacoes;
+  final List<ComentarioModel> comentarios;
+
+  PerfilModel({
+    required this.id,
+    required this.biografia,
+    required this.fotoPerfil,
+    required this.banner,
+    required this.usuario,
+    required this.publicacoes,
+    required this.comentarios,
+  });
+
+  factory PerfilModel.fromJson(Map<String, dynamic> json) {
+    return PerfilModel(
+      id: json['id'],
+      biografia: json['biografia'],
+      fotoPerfil: json['fotoPerfil'],
+      banner: json['banner'],
+      usuario: UsuarioModel.fromJson(json['usuario']),
+      publicacoes:
+          (json['publicacoes'] as List)
+              .map((publicacao) => PublicacaoModel.fromJson(publicacao))
+              .toList(),
+      comentarios:
+          (json['comentarios'] as List)
+              .map((comentario) => ComentarioModel.fromJson(comentario))
+              .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'biografia': biografia,
+      'fotoPerfil': fotoPerfil,
+      'banner': banner,
+      'usuario': usuario.toJson(),
+      'publicacoes':
+          publicacoes.map((publicacao) => publicacao.toJson()).toList(),
+      'comentarios':
+          comentarios.map((comentario) => comentario.toJson()).toList(),
+    };
+  }
+}
