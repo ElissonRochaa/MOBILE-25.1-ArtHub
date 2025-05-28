71ddec1bacdc5d25cdad96c40b1d2484b18f08e6
laahnanes <larahnanes160@gmail.com>
2025-05-28 09:43:41 -0300

    feat: widget de stackbar

--- lib/pages/tela_login.dart
+++ lib/pages/tela_login.dart
@@ -17,33 +17,59 @@ class _TelaLoginState extends State<TelaLogin> {
   final TextEditingController _senhaController = TextEditingController();
 
   void dipose() {
-  void dipose() {
-    _emailController.dispose();
-    _senhaController.dispose();
-    super.dispose();
-  }
+    void dipose() {
+      _emailController.dispose();
+      _senhaController.dispose();
+      super.dispose();
+    }
 
-  Future<void> _login() async {
-    try {
-      final usuarioLogin = LoginDTO(
-        email: _emailController.text,
-        senha: _senhaController.text,
-      );
-      final response = await UsuarioService.login(usuarioLogin);
+    Future<void> _login() async {
+      try {
+        final usuarioLogin = LoginDTO(
+          email: _emailController.text,
+          senha: _senhaController.text,
+        );
+        final response = await UsuarioService.login(usuarioLogin);
 
-      if (response.isNotEmpty) {
-        // Login bem-sucedido
+        if (response.isNotEmpty) {
+          // Login bem-sucedido
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'O login foi realizado com sucesso.',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onSurface,
+                  fontWeight: Theme.of(context).textTheme.bodySmall?.fontWeight,
+                ),
+              ),
+              backgroundColor: Theme.of(context).colorScheme.onError,
+              duration: Duration(seconds: 2),
+              behavior: SnackBarBehavior.floating,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              margin: EdgeInsets.all(30),
+              elevation: 20,
+            ),
+          );
+          Navigator.pushNamed(context, '/home');
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Senha ou email errados')),
+          );
+        }
+      } catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text(
-              'O login foi realizado com sucesso.',
+              'Senha ou email incorretos. Tente novamente.',
               style: TextStyle(
                 color: Theme.of(context).colorScheme.onSurface,
                 fontWeight: Theme.of(context).textTheme.bodySmall?.fontWeight,
               ),
             ),
             backgroundColor: Theme.of(context).colorScheme.onError,
-            duration: Duration(seconds: 2),
+            duration: Duration(seconds: 3),
             behavior: SnackBarBehavior.floating,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
@@ -52,159 +78,135 @@ class _TelaLoginState extends State<TelaLogin> {
             elevation: 20,
           ),
         );
-        Navigator.pushNamed(context, '/home');
-      } else {
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(const SnackBar(content: Text('Senha ou email errados')));
       }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            'Senha ou email incorretos. Tente novamente.',
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.onSurface,
-              fontWeight: Theme.of(context).textTheme.bodySmall?.fontWeight,
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/upe.jpg'),
+                  colorFilter: ColorFilter.mode(
+                    Theme.of(context).colorScheme.primary,
+                    BlendMode.modulate,
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ),
-          backgroundColor: Theme.of(context).colorScheme.onError,
-          duration: Duration(seconds: 3),
-          behavior: SnackBarBehavior.floating,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          margin: EdgeInsets.all(30),
-          elevation: 20,
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10, left: 10),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: BotaoVoltarWidget(),
+                    ),
+                  ),
+                  SizedBox(height: 150),
+                  InputTexto(
+                    label: 'Email',
+                    hintLabel: 'seumelhoremail@gmail.com',
+                    controller: _emailController,
+                  ),
+                  SizedBox(height: 50),
+                  InputTexto(
+                    label: 'Senha',
+                    hintLabel: 'MuitoSecreta',
+                    controller: _senhaController,
+                    ehOculto: true,
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap:
+                            () => {
+                              Navigator.pushNamed(context, '/esqueceu-senha'),
+                            },
+                        child: Text(
+                          'Esqueceu a senha?',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.bodyMedium?.copyWith(
+                            color: Theme.of(context).colorScheme.surface,
+                            decoration: TextDecoration.underline,
+                            decorationColor:
+                                Theme.of(context).colorScheme.surface,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  BotaoEstilizadoWidget(funcao: _login, texto: 'Fazer Login'),
+                  BotaoEstilizadoWidget(funcao: _login, texto: 'Fazer Login'),
+                  SizedBox(height: 50),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Não tem uma conta?',
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          color: Theme.of(context).colorScheme.surface,
+                        ),
+                      ),
+                      SizedBox(width: 3),
+                      GestureDetector(
+                        onTap:
+                            () => {Navigator.pushNamed(context, '/registro')},
+                        child: Text(
+                          'Registre-se!',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleLarge?.copyWith(
+                            color: Theme.of(context).colorScheme.surface,
+                            decoration: TextDecoration.underline,
+                            decorationColor:
+                                Theme.of(context).colorScheme.surface,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50),
+                  Container(
+                    width: 75,
+                    height: 33,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/logo_upe.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       );
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              image: DecorationImage(
-                image: AssetImage('assets/images/upe.jpg'),
-                colorFilter: ColorFilter.mode(
-                  Theme.of(context).colorScheme.primary,
-                  BlendMode.modulate,
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(40),
-                topRight: Radius.circular(40),
-              ),
-            ),
-          ),
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 10, left: 10),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: BotaoVoltarWidget(),
-                  ),
-                ),
-                SizedBox(height: 150),
-                InputTexto(
-                  label: 'Email',
-                  hintLabel: 'seumelhoremail@gmail.com',
-                  controller: _emailController,
-                ),
-                SizedBox(height: 50),
-                InputTexto(
-                  label: 'Senha',
-                  hintLabel: 'MuitoSecreta',
-                  controller: _senhaController,
-                  ehOculto: true,
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap:
-                          () => {
-                            Navigator.pushNamed(context, '/esqueceu-senha'),
-                          },
-                      child: Text(
-                        'Esqueceu a senha?',
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Theme.of(context).colorScheme.surface,
-                          decoration: TextDecoration.underline,
-                          decorationColor:
-                              Theme.of(context).colorScheme.surface,
-                          decorationColor:
-                              Theme.of(context).colorScheme.surface,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 30),
-                BotaoEstilizadoWidget(funcao: _login, texto: 'Fazer Login'),
-                BotaoEstilizadoWidget(funcao: _login, texto: 'Fazer Login'),
-                SizedBox(height: 50),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Não tem uma conta?',
-                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                        color: Theme.of(context).colorScheme.surface,
-                      ),
-                    ),
-                    SizedBox(width: 3),
-                    GestureDetector(
-                      onTap: () => {Navigator.pushNamed(context, '/registro')},
-                      child: Text(
-                        'Registre-se!',
-                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                          color: Theme.of(context).colorScheme.surface,
-                          decoration: TextDecoration.underline,
-                          decorationColor:
-                              Theme.of(context).colorScheme.surface,
-                          decorationColor:
-                              Theme.of(context).colorScheme.surface,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 50),
-                Container(
-                  width: 75,
-                  height: 33,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primaryContainer,
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/logo_upe.png'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
