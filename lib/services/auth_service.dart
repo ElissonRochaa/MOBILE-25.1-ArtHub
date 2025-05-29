370ed5f8d6649657c6d0cb16f4454eb0bd47582a
Murilo Lucena <muriloglucenacanal@gmail.com>
2025-05-28 23:54:58 -0300

    fix: correção no dispose e comentário dos debugs

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:arthub/models/cadastro_model.dart';
 import 'package:arthub/services/token_service.dart';
 
 import '../api/api_client.dart';
@@ -6,19 +7,39 @@ import '../models/dtos/login_dto.dart';
 class AuthService {
   static final ApiClient _apiClient = ApiClient();
 
+  // Aqui o método do Login
   static Future<String> login(LoginDTO login) async {
     final response = await _apiClient.post('/auth/login', {
       'email': login.email,
-      'senha' : login.senha,
+      'senha': login.senha,
     });
 
-    if (response.statusCode == 200){
+    if (response.statusCode == 200) {
       final token = response.data;
       TokenService.saveToken(token);
       return token;
-    }
-    else {
+    } else {
       throw Exception('Falha ao fazer o login');
+    }
+  }
+
+  // Aqui o método do Cadastro
+  static Future<void> cadastrarUsuario(CadastroModel cadastro) async {
+    final response = await _apiClient.post('/auth/registrar', {
+      'nome': cadastro.nome,
+      'apelido': cadastro.apelido,
+      'email': cadastro.email,
+      'senha': cadastro.senha,
+      'telefone': cadastro.telefone,
+      'dataNascimento': cadastro.dataNascimento,
+    });
+
+    //Aqui era só debug
+    // print(response.data);
+    // print(response.statusCode);
+
+    if (response.statusCode != 201) {
+      throw Exception('Falha ao cadastrar usuário');
     }
   }
 }
--- lib/services/usuario_service.dart
+++ lib/services/usuario_service.dart
@@ -5,39 +5,4 @@ import 'package:arthub/services/auth_service.dart';
 
 class UsuarioService {
   static final ApiClient _apiClient = ApiClient();
-
-  static Future<String> login(LoginDTO login) async {
-    final response = await _apiClient.post('/auth/login', {
-      'email': login.email,
-      'senha': login.senha,
-    });
-
-    if (response.statusCode == 200) {
-      final token = response.data;
-      AuthService.saveToken(token);
-      return token;
-    } else if (response.statusCode == 403) {
-      throw Exception('Senha ou e-mail incorretos');
-    } else {
-      throw Exception('Falha ao fazer o login');
-    }
-  }
-
-  static Future<void> cadastrarUsuario(CadastroModel cadastro) async {
-    final response = await _apiClient.post('/auth/registrar', {
-      'nome': cadastro.nome,
-      'apelido': cadastro.apelido,
-      'email': cadastro.email,
-      'senha': cadastro.senha,
-      'telefone': cadastro.telefone,
-      'dataNascimento': cadastro.dataNascimento,
-    });
-
-    print(response.data);
-    print(response.statusCode);
-
-    if (response.statusCode != 201) {
-      throw Exception('Falha ao cadastrar usuário');
-    }
-  }
 }
