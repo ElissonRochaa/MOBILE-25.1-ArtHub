9d884591d8acb33c93e24cc5605460ec632e32e1
Pedro-RGS <pedro.gsouza@upe.br>
2025-05-31 19:36:09 -0300

    fix: getAllPublicacao() funcional

--- lib/services/publicacao_service.dart
+++ lib/services/publicacao_service.dart
@@ -7,13 +7,9 @@ class PublicacaoService {
   static ApiClient _apiClient = ApiClient();
 
   static Future<List<PublicacaoModel>> getAllPublicacao() async {
-    print('chegou até aqui');
     final response = await _apiClient.get('/publicacoes');
-    print(response.statusCode);
-    final List<PublicacaoModel> publicacoes = (response.data as List).map((publicacao) => PublicacaoModel.fromJson(publicacao)).toList();
-    print(publicacoes[1]);
+    List<PublicacaoModel> publicacoes = (response.data as List).map((publicacao) => PublicacaoModel.fromJson(publicacao)).toList();
     if(response.statusCode == 200){
-      print(response.statusCode);
       return publicacoes;
     }
 
