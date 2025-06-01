8b4aa7734abfbc3fb5e5e4016cedec231e86daa3
Pedro-RGS <pedro.gsouza@upe.br>
2025-06-01 00:22:39 -0300

    feat: criação do método getAllPublicacaoByUsuario

--- lib/services/publicacao_service.dart
+++ lib/services/publicacao_service.dart
@@ -9,6 +9,7 @@ class PublicacaoService {
   static Future<List<PublicacaoModel>> getAllPublicacao() async {
     final response = await _apiClient.get('/publicacoes');
     List<PublicacaoModel> publicacoes = (response.data as List).map((publicacao) => PublicacaoModel.fromJson(publicacao)).toList();
+
     if(response.statusCode == 200){
       return publicacoes;
     }
@@ -17,6 +18,13 @@ class PublicacaoService {
   }
 
   static Future<List<PublicacaoModel>> getPublicacaoByUsuario() async {
-    return null
+    final response = await _apiClient.get('/usuario/{idUsuario}');
+    List<PublicacaoModel> pubicacoesDoUsuario = (response.data as List).map((post) => PublicacaoModel.fromJson(post)).toList();
+
+    if(response.statusCode == 200){
+      return pubicacoesDoUsuario;
+    }
+
+    throw Exception('Algo deu errado ao buscar as publicações do usuário');
   }
 }
