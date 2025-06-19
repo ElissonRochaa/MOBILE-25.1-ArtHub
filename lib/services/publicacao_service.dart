338cefbbb0ee33265f38e5dc8bb32e9b4351035e
Pedro-RGS <pedro.gsouza@upe.br>
2025-06-18 23:46:09 -0300

    feat: criação do método getPublicacaoByCategoria

--- lib/services/publicacao_service.dart
+++ lib/services/publicacao_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:arthub/api/api_client.dart';
+import 'package:arthub/enums/categoria_enum.dart';
 import 'package:arthub/models/publicacao_model.dart';
 import 'package:arthub/services/token_service.dart';
 import 'package:arthub/services/usuario_service.dart';
@@ -8,15 +9,6 @@ import 'package:dio/dio.dart';
 
 class PublicacaoService {
   static final ApiClient _apiClient = ApiClient();
-
-  // Future<int> getIdDono() async {
-  //   String email = await TokenService.decodeToken(); //Extrai o email do token
-  //   final usuario = await UsuarioService.getUsuarioByEmail(
-  //     email,
-  //   ); //Busca o usuário pelo email
-
-  //   return usuario.id; //Pega o ID!
-  // }
 
   static Future<List<PublicacaoModel>> getAllPublicacao() async {
     final response = await _apiClient.get('/publicacoes');
@@ -43,6 +35,23 @@ class PublicacaoService {
     }
 
     throw Exception('Algo deu errado ao buscar as publicações do usuário');
+  }
+
+  static Future<List<PublicacaoModel>> getPublicacaoByCategoria(String categoria) async {
+    try{
+      print(categoria);
+      final response = await _apiClient.get('/publicacoes/categoria/$categoria');
+
+      List<PublicacaoModel> publicacoes =
+      (response.data as List)
+          .map((post) => PublicacaoModel.fromJson(post))
+          .toList();
+
+      return publicacoes;
+    }
+    catch (e){
+      throw Exception('Algo deu errado ao buscar as publicações por categoria: $e');
+    }
   }
 
   static Future<Uint8List> getBytes(String idPublicacao) async {
